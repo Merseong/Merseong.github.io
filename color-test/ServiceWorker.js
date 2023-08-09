c15c7fd1851a8c5627c656de91e81b341f236277
Merseong <esc990720@korea.ac.kr>
2023-08-09 07:53:06 +0000

    Update color sweeper test

--- color-test/ServiceWorker.js
+++ color-test/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "ARRKKA-ColorMinesweeper-0.0.421";
+const cacheName = "ARRKKA-ColorMinesweeper-0.0.422";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js",
